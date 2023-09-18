a5419c2128554cafc5532bfd70b80f5f79a1712d
Petya <petya200606@gmail.com>
2023-09-19 01:43:13 +0800

    dzz64

--- dzz64/Program.cs
+++ dzz64/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void FindAndPrintDigits(int num)
+{
+    if (num == 0) return;
+    if (num == 1)
+    Console.WriteLine($"{num}");
+
+    else Console.Write($"{num}, ");
+    FindAndPrintDigits(num - 1);
+}
+
+FindAndPrintDigits(num);
