af42ade58c30908bc52fa60239e127ee03809724
Petya <petya200606@gmail.com>
2023-09-19 01:44:10 +0800

    dzz66

---
+++ dzz66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Write("M = ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("N = ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int FindDistBetwFrstScnd(int num1, int num2)
+{
+
+  if (num1 == num2) return num2;
+  else return num1 + FindDistBetwFrstScnd(num1+1,num2);
+
+}
+
+
+if (num1 < num2)
+Console.WriteLine($"-> {FindDistBetwFrstScnd(num1,num2)}");
+if (num1 > num2)
+Console.WriteLine($"-> {FindDistBetwFrstScnd(num2,num1)}");
